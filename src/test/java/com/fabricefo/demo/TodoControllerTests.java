62d224e455a6f8ffdf77fbf77dadcfc2362d7d4d
Fabrice Fourel <ffourel@gmail.com>
2023-10-31 15:50:54 +0100

    Add class test for controller

---
+++ src/test/java/com/fabricefo/demo/TodoControllerTests.java
@@ -0,0 +1,10 @@
+package com.fabricefo.demo;
+
+import com.fabricefo.demo.controller.TodoController;
+import com.fabricefo.demo.model.Todo;
+import com.fabricefo.demo.repository.TodoRepository;
+
+@WebMvcTest(TodoController.class)
+public class TodoControllerTests {
+    
+}
